1699d0ccdbd734edc5ffab6e64908a860f26cbe2
Aron Heinecke <aron.heinecke@t-online.de>
2022-06-15 22:53:20 +0200

    add docs and migrate watcherkind example

--- examples/watcher_kind/src/main.rs
+++ examples/watcher_kind/src/main.rs
@@ -1,15 +1,28 @@
-use std::time::Duration;
+use std::{path::Path, time::Duration};
 
 use notify::{poll::PollWatcherConfig, *};
 fn main() {
-    let (tx, _rx) = std::sync::mpsc::channel();
-    let _watcher: Box<dyn Watcher> = if RecommendedWatcher::kind() == WatcherKind::PollWatcher {
+    let (tx, rx) = std::sync::mpsc::channel();
+
+    let mut watcher: Box<dyn Watcher> = if RecommendedWatcher::kind() == WatcherKind::PollWatcher {
+        // custom config for PollWatcher kind
         let config = PollWatcherConfig {
             poll_interval: Duration::from_secs(1),
             ..Default::default()
         };
         Box::new(PollWatcher::with_config(tx, config).unwrap())
     } else {
+        // use default for everything else
         Box::new(RecommendedWatcher::new(tx).unwrap())
     };
+
+    // watch some stuff
+    watcher
+        .watch(Path::new("."), RecursiveMode::Recursive)
+        .unwrap();
+
+    // print all events, this blocks forever
+    for e in rx {
+        println!("{:?}", e);
+    }
 }
